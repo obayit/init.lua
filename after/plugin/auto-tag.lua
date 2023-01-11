1632f62efbb02f4923d3f029c89aa2fb8854db4d
Obay Abdelgadir <obayit@gmail.com>
2023-01-11 13:19:55 +0200

    [ADD] my snippets 🥳

---
+++ after/plugin/auto-tag.lua
@@ -0,0 +1,10 @@
+require('nvim-ts-autotag').setup {
+    filetypes = {
+        'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx',
+        'rescript',
+        'xml',
+        'php',
+        'markdown',
+        'glimmer', 'handlebars', 'hbs'
+    },
+}
--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -10,11 +10,13 @@ vim.keymap.set({ "i", "s" }, "<Tab>", function()
     ls.jump(1)
   end
 end, { silent = true })
-require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
-
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_vscode").load({ paths = { "~/.config/nvim/snippets" } })
+-- require("luasnip.loaders.from_snipmate").load({ paths = { "~/.config/nvim/snippets" } })
+--
 -- " For changing choices in choiceNodes (not strictly necessary for a basic setup).
 -- imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
 -- smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
 --
-vim.keymap.set("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>")
+vim.keymap.set("n", "<leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>")
 
