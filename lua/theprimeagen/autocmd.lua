13b94619196a0018a879dacedd642c0570df6297
Obay Abdelgadir <obayit@gmail.com>
2023-02-05 17:01:42 +0200

    [ADD] 2 indentation for javascript

--- lua/theprimeagen/autocmd.lua
+++ lua/theprimeagen/autocmd.lua
@@ -5,3 +5,8 @@ autocmd("FocusLost", {
     pattern = "*",
     command = "wa"
 })
+
+autocmd('Filetype', {
+  pattern = { 'javascript', 'typescript' },
+  command = 'setlocal tabstop=2 shiftwidth=2'
+})
