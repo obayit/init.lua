b50ac8aee8dfa944f5f800ab64971824da060da9
Ubay Abdelgadir <obayit@gmail.com>
2023-10-11 18:58:54 +0200

    minor fixes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,21 +16,22 @@ lsp.configure('lua_ls', {
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
+cmp.setup({
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
+    -- disable completion with tab
+    ['<Tab>'] = nil,
+    ['<S-Tab>'] = nil,
+  })
 })
 
--- disable completion with tab
--- this helps with copilot setup
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
-
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
 
 lsp.set_preferences({
     suggest_lsp_servers = false,
